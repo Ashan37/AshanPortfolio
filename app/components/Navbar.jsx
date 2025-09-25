3246e6e08471f83673aa294815741b8242c91522
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-09-25 12:06:05 +0530

    add tailwind.config.js, postcss.config.js and updated frontend of the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,25 +2,48 @@ import React from 'react'
 import Image from 'next/image'
 import { assets } from '../assets/assets'
 
+
 const Navbar = () => {
   return (
     <>
-    <nav>
-        <a href="">
-            <Image src={assets.ashan} alt='Ashan' className='w-28 cursor-pointer mr-14' />
+    <div className='fixed top-0 right-0 w-11/12 z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} className='w-full'/>
+    </div>
+    <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+        <a href="#top">
+            <Image src={assets.logo} alt='Ashan' className='w-28 cursor-pointer mr-14' />
         </a>
 
-        <ul>
-            <li><a href="#top">Home</a></li>
-            <li><a href="#about">About me</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#work">My work</a></li>
-            <li><a href="#contact">Contact me</a></li>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white bg-opacity-50'>
+            <li><a className='font-Ovo' href="#top">Home</a></li>
+            <li><a className='font-Ovo' href="#about">About me</a></li>
+            <li><a className='font-Ovo' href="#services">Services</a></li>
+            <li><a className='font-Ovo' href="#work">My work</a></li>
+            <li><a className='font-Ovo' href="#contact">Contact me</a></li>
         </ul>
 
-        <div>
-            <a href="#contact">Contact<Image src={assets.arrow_icon} alt='arrow' className="w-3"/></a>
+
+        <div className='flex items-center gap-4'>
+          <button>
+            <Image src={assets.moon_icon} className=''/>
+          </button>
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact<Image src={assets.arrow_icon} alt='arrow' className="w-3"/></a>
+         <button className='block md:hidden ml-3'>
+            <Image src={assets.menu_black} className=''/>
+          </button>
         </div>
+        {/* ----------mobile view---------- */}
+        <ul className='flex md:hidden flex-col gap-4 py-20 px-10 fixed right-0 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+          <div className='absolute right-6 top-6'>
+            <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+          </div>
+            <li><a className='font-Ovo' href="#top">Home</a></li>
+            <li><a className='font-Ovo' href="#about">About me</a></li>
+            <li><a className='font-Ovo' href="#services">Services</a></li>
+            <li><a className='font-Ovo' href="#work">My work</a></li>
+            <li><a className='font-Ovo' href="#contact">Contact me</a></li>
+        </ul>
     </nav>
       
     </>
